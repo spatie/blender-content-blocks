bb2e730733f269bb200849821ace1eed427d2cda
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-10 16:58:52 +0100

    bye jquery

---
+++ src/components/forms/Select.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <select
+            :value="value" 
+            @change="$emit('input', $event.target.value)"
+        >
+            <option
+                v-for="(label, value) in options"
+                :value="value"
+            >
+                {{ label }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props: ['value'],
+};
+</script>
